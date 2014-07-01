d102db0a528399cd4da526f7a7faaf20a0f20bc7
Luke Drummond <lukedrummond@gmail.com>
2014-07-01 01:25:02 +0100

    Added options parser allowing selection of Lua and LuaRocks version, as well as installation directories.
    Improved error handling and error messages.

--- luavirtualenv.sh
+++ luavirtualenv.sh
@@ -9,43 +9,103 @@
 LUAROCKS_TAR_GZ_URL="http://www.luarocks.org/releases/luarocks-2.1.2.tar.gz"
 LUAROCKS_VERSION_STRING="2.1.2"
 LUA_INCLUDE_PATH=/usr/include/
-
-if [ -n $TMPDIR ] ; then
-    SCRATCH=/tmp
-else
-    SCRATCH=$TMPDIR
-fi    
-
-INSTALL_DIR=$PWD
 SYS_LUA=`which lua`
+INSTALL_DIR=$PWD
+
+usage(){
+cat <<EOF
+Available options:
+    -r <version>    install luarocks version=<version> (default: $LUAROCKS_VERSION_STRING)
+    -l <path>       use lua installation from <path> (default: `which lua`)
+    -d <dir>        create lua virtualenv in <dir> (defaults to the working directory)
+EOF
+}
+
+err_exit(){
+cat <<EOF
+==========================================
+An error occurred and installation failed:
+==========================================
+$1
+------------------------------------------
+EOF
+    exit 1
+}
+
+RE="^[0-9]+.[0-9]+.[0-9]+\w*[-]?$"
+while getopts ":r:l:d:" opt; do
+    case $opt in
+        r)
+            if [[ "$OPTARG" =~ $RE ]]; then
+                LUAROCKS_VERSION_STRING="$OPTARG"
+            else
+                err_exit "'$OPTARG' does not appear to be a valid luaRocks version"
+            fi
+            echo $LUAROCKS_TAR_GZ_URL
+            LUAROCKS_TAR_GZ_URL="http://www.luarocks.org/releases/luarocks-$LUAROCKS_VERSION_STRING.tar.gz"
+        ;;
+        l)
+            SYS_LUA="$OPTARG"
+            if ! [ -f "$SYS_LUA" ] ; then
+                err_exit "$SYS_LUA does not exist"
+            fi
+        ;;
+        d)
+            INSTALL_DIR="$OPTARG"
+            if ! [ -d "$INSTALL_DIR" ] ; then
+                mkdir -p "$INSTALL_DIR" || err_exit "Couldn't create $INSTALL_DIR"
+            fi
+        ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2 ; usage 1
+            exit 1
+        ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2 ; usage 1
+            exit 1
+        ;;
+    esac
+done
+
+echo "installing in $INSTALL_DIR"
 
 if [ ! -n "$SYS_LUA" ] ; then
-    echo "Couldn't detect Lua installation"  >&2 
+    err_exit "Couldn't detect Lua installation"
 fi
 
 LUA_VERSION=`$SYS_LUA -e 'print(_VERSION:match("%d.%d$"))'`
 if [ ! -n "$LUA_VERSION" ] ; then 
-    echo "Could not detect system Lua version" >&2
-    exit 1
+    err_exit "Could not detect system Lua version"
 fi
 
+if ! [ "$(ls -A $INSTALL_DIR 2> /dev/null)" == "" ]; then
+    Y=""
+    read -p "$INSTALL_DIR is not empty:continue anyway?(yN)" Y
+    if  [ "$Y" != 'y' ] ; then
+        err_exit "ABORTING..."
+    fi
+fi
 
-mkdir -p "$INSTALL_DIR/bin" || echo "mkdir failed"
+mkdir -p "$INSTALL_DIR/bin" || err_exit "mkdir failed"
 cp "$SYS_LUA" "$INSTALL_DIR/bin/lua"
-echo "installing luarocks"
-BUILD_DIR=`tempfile -d $SCRATCH`
+echo "installing luarocks into $BUILD_DIR..."
 
+BUILD_DIR=`mktemp -d `
 cd "$BUILD_DIR"
-wget $LUAROCKS_TAR_GZ_URL -O - | tar -xzf - 
+wget "$LUAROCKS_TAR_GZ_URL" -O - | tar -xzf - || err_exit "Couldn't fetch $LUAROCKS_TAR_GZ_URL"
 cd "luarocks-$LUAROCKS_VERSION_STRING"
 
-./configure  --prefix="$INSTALL_DIR" --rocks-tree="$INSTALL_DIR" --lua-version="$LUA_VERSION" --with-lua="$INSTALL_DIR" \
---sysconfdir="$INSTALL_DIR/luarocks" --force-config --with-lua-include="$LUA_INCLUDE_PATH"
-make build && make install
+#configure
+(./configure  --prefix="$INSTALL_DIR" --rocks-tree="$INSTALL_DIR" \
+--lua-version="$LUA_VERSION" --with-lua="$INSTALL_DIR" \
+--sysconfdir="$INSTALL_DIR/luarocks" --force-config \
+--with-lua-include="$LUA_INCLUDE_PATH") || err_exit "failed to configure"
+
+(make build && make install) || err_exit "LuaRocks build failed"
 
 cd "$INSTALL_DIR"
 ./bin/luarocks path > bin/activate
 echo "export PATH=$INSTALL_DIR/bin/:\$PATH" >> bin/activate
-rm -R "$INSTALL_DIR/luarocks-$LUAROCKS_VERSION_STRING"
+rm -R "$BUILD_DIR/luarocks-$LUAROCKS_VERSION_STRING"
 rm -R "$BUILD_DIR"
 
